95aa6b5c0a9cfc605ca4ce71179fe975cb3b9f3f
darwin1211 <77988437+darwin1211@users.noreply.github.com>
2023-04-21 05:45:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = socket.listen(server);
-var port = process.env.PORT || 1111;
+var port = process.env.PORT || 2222;
 server.listen(port, function () {
   console.log("Server listening at port %d", port);
 });
